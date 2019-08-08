5e4cba59604383243a68f35ec5bd00ee2d9915d6
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-08 05:08:46 -0400

    Updated Python script for generating the build lists

--- tools/builds.py
+++ tools/builds.py
@@ -10,14 +10,19 @@ def main():
     The function that parses the FiveM page into
     JSON that LambentLight/ServerManager can read.
     """
-    # First we need to check that the file with the HTML exists
-    if not os.path.isfile("fivem.html"):
-        # Print a message and exit with a code 2
-        print("The file with the builds does not exists!")
+    # If the number of argument is not three
+    if len(sys.argv) != 3:
+        print("Wrong number of arguments. Expected 3, got " + len(sys.argv))
         sys.exit(2)
 
+    # First we need to check that the file with the HTML exists
+    if not os.path.isfile(sys.argv[1]):
+        # Print a message and exit with a code 2
+        print("The file with the builds does not exists!")
+        sys.exit(3)
+
     # Load the contents into the lxml parser
-    html = lxml.html.parse("fivem.html")
+    html = lxml.html.parse(sys.argv[1])
     # Get the a nodes
     a_nodes = html.xpath("//a")
 
@@ -32,7 +37,7 @@ def main():
             builds.append(node.attrib["title"])
 
     # Open a file for writing the builds
-    with open("builds.json", "w") as output:
+    with open(sys.argv[2], "w") as output:
         # Dump the list of builds
         json.dump(builds, output, indent=4)
         # And finally and a line at the end
