5e5526ff0cad9d44543941995e7893aceb422a68
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-27 22:05:12 -0300

    You can now choose specific types of content to generate

--- tools/__main__.py
+++ tools/__main__.py
@@ -1,3 +1,5 @@
+import sys
+
 from .builds import generate_builds
 
 
@@ -5,8 +7,18 @@ def main():
     """
     Runs all of the Metadata generation logic.
     """
-    # Generate the list of builds
-    generate_builds()
+    # If the number of arguments is lower than two (script name + mode)
+    if len(sys.argv) < 2:
+        # Set the mode to all
+        mode = "all"
+    # Otherwise
+    else:
+        # Set the mode to the one specified by the user
+        mode = sys.argv[1]
+
+    # IF the mode is set to all or builds, generate the list of builds
+    if mode == "all" or mode == "builds":
+        generate_builds()
 
 
 if __name__ == "__main__":
