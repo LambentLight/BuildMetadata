cf7adfc9a8fcb1a90ed3d66407684912b91de7df
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-27 23:45:01 -0300

    Added function for getting the list of releases from a GitHub Repo

---
+++ tools/github.py
@@ -0,0 +1,35 @@
+import requests
+
+API_RELEASES = "https://api.github.com/repos/{0}/{1}/releases"
+
+
+def get_releases(owner, repo):
+    """
+    Gets a list of releases from a GitHub Repository.
+    """
+    # Create a place for storing the releases
+    releases = []
+    # Make the GET request
+    get = requests.get(API_RELEASES.format(owner, repo))
+
+    # If the request is not 200, return the empty list
+    if get.status_code != 200:
+        return releases
+
+    # If is 200, iterate over the releases
+    for release in get.json():
+        # If assets is empty, set the zip as the file
+        if not release["assets"]:
+            download = release["zipball_url"]
+        # If there is a released file, save the first one it
+        else:
+            download = release["assets"][0]["browser_download_url"]
+
+        # Then, get the version of the release with the v stripped
+        version = release["tag_name"].strip("v")
+
+        # And add the release onto the list
+        releases.append({"version": version, "download": download})
+
+    # Finally, return the new list of releases
+    return releases
