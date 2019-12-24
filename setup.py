28d70b600e2ccd2cf28deea9649a29c58d48a2fd
Hannele Ruiz <justlemoncl@gmail.com>
2019-12-24 17:54:32 -0300

    Added Python Package setup instructions

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="lambentlight",
+    version="0.1",
+    author="Hannele Ruiz",
+    author_email="lambentlight@justalemon.ml",
+    description="LambentLight Development Tools",
+    long_description="LambentLight Development Tools",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LambentLight/metadata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
